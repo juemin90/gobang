a164266da0cc7d947016a3f73279622cb79b6a51
JueminLu <jmlu0619@gmaili.com>
2017-06-21 08:53:16 +0800

    add piece

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+var PIECE_NUM = 0;
+var PIECE_COORDINATE = [];
+
 $(function () {
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
@@ -5,6 +8,7 @@ $(function () {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   init(ctx);
+  initEvent(ctx);
 })
 
 function init(ctx) {
@@ -33,13 +37,80 @@ function init(ctx) {
   dots.forEach(d1 => {
     dots.forEach(d2 => {
       ctx.beginPath();
-      var start = 20 + (d1 - 1) * 30;
-      var end = 20 +(d2 - 1) * 30;
+      var x = 20 + (d1 - 1) * 30;
+      var y = 20 +(d2 - 1) * 30;
 
-      ctx.arc(start, end, 5, 0, 360, false);
+      ctx.arc(x, y, 5, 0, 360, false);
       ctx.fillStyle = "black";
       ctx.fill();
       ctx.closePath();
     })
   })
+}
+
+function initEvent(ctx) {
+  var canvas = document.getElementById('canvas');
+  canvas.addEventListener('click', function (evt) {
+    var mousePos = getMousePos(canvas, evt)
+    addPiece(mousePos, ctx);
+  });
+}
+
+function getMousePos(canvas, evt) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top
+  };
+}
+
+function addPiece(mousePos, ctx) {
+  if (mousePos.x < 20 || mousePos.y < 20 || mousePos.x > 440 || mousePos.y > 440) return;
+
+  var pieceCoordinate = getPieceCoordinate(mousePos);
+  if (PIECE_COORDINATE.find(function (c) {
+    return c.x === pieceCoordinate.x && c.y === pieceCoordinate.y;
+  })) return;
+
+  var x = 20 + 30 * (pieceCoordinate.x - 1);
+  var y = 20 + 30 * (pieceCoordinate.y - 1);
+
+  ctx.beginPath();
+  ctx.arc(x, y, 13, 0, 360, false);
+  ctx.fillStyle = PIECE_NUM % 2 === 1 ? "black" : 'white';
+  ctx.fill();
+  ctx.closePath();
+
+  PIECE_NUM++;
+  PIECE_COORDINATE.push(pieceCoordinate);
+  console.log(PIECE_COORDINATE);
+
+  if (!judge()) return;
+  if (judge() === 1) {
+    setTimeout(function () {
+      alert('black wins');
+      init(ctx);
+    }, 100)
+  } else if (judge() === 0) {
+    setTimeout(function () {
+      alert('white wins');
+      init(ctx);
+    }, 100)
+  }
+}
+
+function getPieceCoordinate(mousePos) {
+  var x =  Math.round((mousePos.x - 20) / 30) + 1;
+  var y =  Math.round((mousePos.y - 20) / 30) + 1;
+
+  return { x: x, y: y, piece_type: PIECE_NUM % 2 };
+}
+
+function judge() {
+  PIECE_COORDINATE.forEach(function (c) {
+
+  })
+  if (PIECE_NUM === 5) {
+    return 1;
+  }
 }
